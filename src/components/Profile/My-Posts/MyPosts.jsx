34abdca69086bf41e0fb7de412aa354090373cd8
Dmytro733 <ddp0496@gmail.com>
2022-03-22 01:46:47 +0200

    stidied props for components

--- src/components/Profile/My-Posts/MyPosts.jsx
+++ src/components/Profile/My-Posts/MyPosts.jsx
@@ -14,7 +14,9 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={style.post_items}>
-        <Post/>
+        <h3 className={style.posts_header}>Customers posts</h3>
+        <Post avatar="https://historyurok.com.ua/wp-content/uploads/2020/01/Aktor.jpg" name="Will Smith" message="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
+        <Post avatar="https://www.themoviedb.org/t/p/w235_and_h235_face/wo2hJpn04vbtmh0B9utCFdsQhxM.jpg" name="Leonardo Dicaprio" message="Lorem adipisicing elit."/>
       </div>
     </div>
   );
--- src/components/Profile/My-Posts/Post/Post.jsx
+++ src/components/Profile/My-Posts/Post/Post.jsx
@@ -1,8 +1,27 @@
 import style from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
   return (
-    <div className={style.post_item}>post item</div>
+    <div className={style.post_item}>
+      <div className={style.post_avatar}>
+        <div>
+          <img src={props.avatar} alt="" />
+        </div>
+      </div>
+      <div className={style.post_content}>
+        <div className={style.post_name}>
+          <span>{props.name}</span>
+        </div>
+        <div className={style.post_text}>
+          {props.message}
+        </div>
+        <div className={style.post_buttons}>
+          <div className={style.post_like}>
+            Like
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
