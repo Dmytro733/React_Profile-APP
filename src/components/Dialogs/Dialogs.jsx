e55942fa3e3ecafb929a1da055f7df6d392e6470
Dmytro733 <ddp0496@gmail.com>
2022-04-21 00:49:43 +0300

    onClick, ref, VirtualDOM

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import DialogItem from "./DialogItem/DialogItem";
 import style from "./Dialogs.module.css";
 import MessageItem from "./Message/Message";
@@ -7,6 +8,13 @@ const Dialogs = (props) => {
   let DialogsElements = props.DirectPage.DialogsData.map(dialog => <DialogItem key={dialog.id} name={dialog.name} id={dialog.id} avatar={dialog.avatar} />)
   let MessagesElements = props.DirectPage.MessagesData.map(message => <MessageItem key={message.id} message={message.message} id={message.id} />)
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text)
+  }
+
   return (
     <div className={style.dialogs_wrap}>
       <div className={style.dialogs}>
@@ -15,6 +23,10 @@ const Dialogs = (props) => {
         </div>
         <div className={style.messages}>
           {MessagesElements}
+          <div className={style.typing_message}>
+            <textarea ref={newPostElement} rows="2"></textarea>
+            <button  onClick={addPost}>Send</button>
+          </div>
         </div>
       </div>
     </div>
