06cfe4fb79b9a0128df94d754ac8f6c8f6fb3b1c
Dmytro733 <ddp0496@gmail.com>
2022-06-26 00:17:51 +0300

    studyed API use axios

--- src/components/Finde-Users/UserItem/UserItem.jsx
+++ src/components/Finde-Users/UserItem/UserItem.jsx
@@ -9,8 +9,12 @@ const UserItem = (props) => {
     <div className={style.user_item_wrap}>
       <div className={style.follow_and_avatar}>
         <div className={style.user_avatar}>
-          <img src={props.avatar} alt="" />
-        </div>
+          {
+            props.avatar == undefined
+            ? <img src="https://images.unsplash.com/photo-1470441623172-c47235e287ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="" />
+            : <img src={props.avatar} alt="" />
+          }
+        </div> 
         <button type="button" className={style.to_follow} onClick={()=>{props.toogleFollowFunction(props.id)}}>
           {props.followed ? "unfollow" : "follow"}
         </button>
@@ -21,10 +25,23 @@ const UserItem = (props) => {
         </NavLink>
         <div className={style.user_location}>
           <span></span>
-          {props.location.country}, {props.location.city} 
+          {
+            props.location.city 
+            ? props.location.city
+            : "London"
+          }
+          {
+            props.location.street 
+            ? props.location.street
+            : "Kulas Light"
+          }
         </div>
         <div className={style.user_status}>
-          {props.status}
+          {
+            props.status == undefined 
+            ? "I`m boss"
+            : props.status
+          }
         </div>
       </div>
     </div>
--- src/components/Finde-Users/Users.jsx
+++ src/components/Finde-Users/Users.jsx
@@ -1,57 +1,20 @@
 import React from "react";
 import style from "./Users.module.css";
 import UserItem from "./UserItem/UserItem";
+import * as axios from "axios";
 
 
 const Users = (props) => {
-  let UsersElements = props.usersData.map(user => <UserItem key={user.id} name={user.name} id={user.id} avatar={user.avatar} location={user.location} status={user.status} followed={user.followed} toogleFollowFunction={props.toogleFollow} />)
+  // let UsersElements = props.usersData.map(user => <UserItem key={user.id} name={user.name} id={user.id} avatar={user.avatar} location={user.location} status={user.status} followed={user.followed} toogleFollowFunction={props.toogleFollow} />)
+  let UsersElements = props.usersData.map(user => <UserItem key={user.id} name={user.name} id={user.id} avatar={user.avatar} location={user.address} status={user.status} followed={user.followed} toogleFollowFunction={props.toogleFollow} />)
 
-  let usersAPI = [
-    {
-      id: 1, 
-      name: "Janet", 
-      avatar: "https://images.unsplash.com/photo-1470441623172-c47235e287ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
-      location: {
-        country: "Ukraine",
-        city: "Ivano-Frankovsk",
-      },
-      status: "I`m boss",
-      followed: true
-    },
-    {
-      id: 2, 
-      name: "Dima", 
-      avatar: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-      location: {
-        country: "English",
-        city: "London",
-      },
-      status: "I`m boss too",
-      followed: true
-    },
-    {
-      id: 3, 
-      name: "Anna", 
-      avatar: "https://images.unsplash.com/photo-1509967419530-da38b4704bc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1195&q=80",
-      location: {
-        country: "USA",
-        city: "California",
-      },
-      status: "I`m boss too",
-      followed: false
-    },
-    {
-      id: 4, 
-      name: "Volodimir", 
-      avatar: "https://images.unsplash.com/photo-1565464027194-7957a2295fb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
-      location: {
-        country: "Ukraine",
-        city: "Ivano-Frankovsk",
-      },
-      status: "I`m boss too",
-      followed: false
-    }
-  ]
+
+  if(props.usersData.length == 0){
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      props.setUsers(response.data);
+    })
+  }
 
   return (
     <div className={style.users_wrap}>
@@ -62,7 +25,7 @@ const Users = (props) => {
         <div className={style.show_more}>
           {
             props.usersData.length == 0 
-            ? <button onClick={()=>{props.setUsers(usersAPI)}}>Get Users</button>
+            ? <button onClick={props.setUsers}>Get Users</button>
             : <button>show more</button>  
           }
         </div>
