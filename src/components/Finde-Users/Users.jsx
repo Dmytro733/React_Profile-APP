8e891bd8294df8b24c3008850b8125a931da0d35
Dmytro733 <ddp0496@gmail.com>
2022-06-28 21:13:23 +0300

    created class component

--- src/components/Finde-Users/Users.jsx
+++ src/components/Finde-Users/Users.jsx
@@ -4,34 +4,34 @@ import UserItem from "./UserItem/UserItem";
 import * as axios from "axios";
 
 
-const Users = (props) => {
-  // let UsersElements = props.usersData.map(user => <UserItem key={user.id} name={user.name} id={user.id} avatar={user.avatar} location={user.location} status={user.status} followed={user.followed} toogleFollowFunction={props.toogleFollow} />)
-  let UsersElements = props.usersData.map(user => <UserItem key={user.id} name={user.name} id={user.id} avatar={user.avatar} location={user.address} status={user.status} followed={user.followed} toogleFollowFunction={props.toogleFollow} />)
+class Users extends React.Component {
 
+  constructor(props) {
+    super(props)
 
-  if(props.usersData.length == 0){
     axios.get("https://jsonplaceholder.typicode.com/users")
     .then(response => {
-      props.setUsers(response.data);
+      this.props.setUsers(response.data);
     })
   }
 
-  return (
-    <div className={style.users_wrap}>
-      <div className={style.users}>
-        <div className={style.users_items}>
-          {UsersElements}
-        </div>
-        <div className={style.show_more}>
-          {
-            props.usersData.length == 0 
-            ? <button onClick={props.setUsers}>Get Users</button>
-            : <button>show more</button>  
-          }
+  render(){
+    return(
+      <div className={style.users_wrap}>
+        <div className={style.users}>
+          <div className={style.users_items}>
+            {
+              this.props.usersData.map(user => <UserItem key={user.id} name={user.name} id={user.id} avatar={user.avatar} location={user.address} status={user.status} followed={user.followed} toogleFollowFunction={this.props.toogleFollow} />)
+
+            }
+          </div>
+          <div className={style.show_more}>
+            <button>show more</button>  
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    )
+  }  
+}
 
 export default Users;
