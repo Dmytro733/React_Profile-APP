292713fddb8e7e792417665c232d7c7eb209e4d6
Dmytro733 <ddp0496@gmail.com>
2022-06-29 14:25:13 +0300

    componentDidMounth method, life cycle method

--- src/components/Finde-Users/Users.jsx
+++ src/components/Finde-Users/Users.jsx
@@ -8,7 +8,9 @@ class Users extends React.Component {
 
   constructor(props) {
     super(props)
+  }
 
+  componentDidMount(){
     axios.get("https://jsonplaceholder.typicode.com/users")
     .then(response => {
       this.props.setUsers(response.data);
@@ -22,7 +24,6 @@ class Users extends React.Component {
           <div className={style.users_items}>
             {
               this.props.usersData.map(user => <UserItem key={user.id} name={user.name} id={user.id} avatar={user.avatar} location={user.address} status={user.status} followed={user.followed} toogleFollowFunction={this.props.toogleFollow} />)
-
             }
           </div>
           <div className={style.show_more}>
